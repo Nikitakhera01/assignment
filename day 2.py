efdd64f520c073db4bfd9e0b1812e701b51fa69b
Nikitakhera01 <67292466+Nikitakhera01@users.noreply.github.com>
2021-09-08 22:51:12 +0530

    Add files via upload
    day 2 assignment submitted

---
+++ day 2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+import numpy as np
+ar=np.array([[1, 2, 3, 4, 5],
+       [2, 3, 4, 5, 6],
+       [4, 5, 6, 7, 8]])
+
+
+# In[8]:
+
+
+ar[1:,1:3]
+
+
+# In[ ]:
+
+
+
+
