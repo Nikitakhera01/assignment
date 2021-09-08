6bd7048b8eb94780a895c42976d0e758a248500c
Nikitakhera01 <67292466+Nikitakhera01@users.noreply.github.com>
2021-09-08 21:53:48 +0530

    Add files via upload
    Day 1 Assignment Submitted

---
+++ day 1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+a=10
+b=20
+print(a*b)
+print(a+b)
+print(a-b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+print(((a+b)*2)%5)
+
+
+# In[1]:
+
+
+principal = float(input('Enter the principle amount: '))
+time = float(input('Enter the time: '))
+rate = float(input('Enter the rate: '))
+simple_interest = (principal*time*rate)/100
+print("Simple interest is:", simple_interest)
+
+
+# In[ ]:
+
+
+
+
